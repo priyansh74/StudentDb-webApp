6ae02a740efc4aa97762bd1e5ac1daf93a1d8275
priyansh74 <priyanshjha99@gmail.com>
2022-02-27 20:11:26 +0530

    Added data validation in modal class

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace StudentDb.Models
 {
     public class Student
     {
+       
         public int StudentId { get; set; }
+        [RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Only Alphabets are allowed in names.")]
+        [StringLength(25)]
         public string Name { get; set;}
+        [Range(1900, 2022)]
         public string Year { get; set;}
     }
 }
